36fca52f999fe7575cc1780f19b85ba5b27665fb
Robiq <robin@wub.no>
2017-01-19 23:51:09 +0900

    Trying gevent

--- mito/mito/mito.py
+++ mito/mito/mito.py
@@ -12,7 +12,7 @@ from my_database import *
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
 # the best option based on installed packages.
-async_mode = "gevent"
+async_mode = "threading"
 
 app = Flask(__name__)
 socketio = SocketIO(app, async_mode=async_mode)	#load socketio
